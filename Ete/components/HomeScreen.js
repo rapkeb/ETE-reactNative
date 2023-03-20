5c5fbef5d00f1b5ad67da0c15b1554e3cff12e9d
omrigoldberg <omrigo13@gmail.com>
2023-03-20 20:21:48 +0200

    support display speech as text on the application screen

--- Ete/components/HomeScreen.js
+++ Ete/components/HomeScreen.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import axios from 'axios';
-import {ImageBackground, View, Text, StyleSheet, Alert} from "react-native";
+import {ImageBackground, View, Text, StyleSheet, Alert, TextInput} from "react-native";
 import Button from "../Button"
 import CircleButton from "../CircleButton";
+//import {PythonShell} from 'python-shell';
+//import $ from 'jquery-ajax';
+//import RNFS from 'react-native-fs';
+//import { usePython } from 'react-py'
 
 const BackgroundImage = require('../assets/BackgroundImage.jpg');
 
-const onTapToHear = () => {
-    // we will implement this later
-        axios.get('http://127.0.0.1:5000/weather', {
-//        axios.get('http://192.168.68.57:5000/weather', {
-          params: {
-            location: 'San Francisco'
-          }
-        })
-        .then(response => {
-//           console.log(response.data);
-            Alert.alert(response.data);
-           // handle response data here
-         })
-        .catch(error => {
-          console.log(error);
-        });
-};
+//const { spawn } = require('child_process');
 
 export default function HomeScreen({ navigation }) {
+    const [displayText, setDisplayText] = useState('');
+
+    const onTapToHear = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:5000/weather');
+            setDisplayText(response.data);
+            clearDisplayText();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const clearDisplayText = () => {
+        setTimeout(() => {
+          setDisplayText('');
+        }, 4000);
+    };
+
     return (
         <View style={styles.container}>
             <ImageBackground source={BackgroundImage} resizeMode="cover" style={styles.image}>
@@ -36,10 +42,58 @@ export default function HomeScreen({ navigation }) {
                 <CircleButton label="Tap To Hear" onPress={onTapToHear} />
                 <Text style={styles.midText1}>Tap to</Text>
                 <Text style={styles.midText2}>Hear</Text>
+                <TextInput style={styles.textBox} value={displayText} multiline={true}/>
             </ImageBackground>
         </View>
     );
 }
+
+//export default function HomeScreen({ navigation }) {
+//    const [displayText, setDisplayText] = useState('');
+//
+//    const onTapToHear = async () => {
+//        try {
+//                $.ajax({
+//                    type: 'POST',
+//                    url: '/Users/omrigoldberg/Desktop/ETE-reactNative/Ete/assets/main.py'
+//                })
+//            console.log("hello world");
+//                <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+//                <py-env>
+//                  - paths:
+//                    - /Users/omrigoldberg/Desktop/ETE-reactNative/Ete/assets/example.py
+//                </py-env>
+//                <py-script>
+//                    from example import generate_random_number
+//                    pyscript.write('lucky', generate_random_number())
+//                </py-script>
+//        } catch (error) {
+//            console.error(error);
+//        }
+//    };
+//
+//    const clearDisplayText = () => {
+//        setTimeout(() => {
+//          setDisplayText('');
+//        }, 4000);
+//    };
+//
+//    return (
+//        <View style={styles.container}>
+//            <ImageBackground source={BackgroundImage} resizeMode="cover" style={styles.image}>
+//                <View style={styles.headerContainer}>
+//                    <Button theme="primary" label="Home" onPress={() => navigation.navigate("Home")} />
+//                    <Button theme="primary" label="Language" onPress={() => navigation.navigate("Language")} />
+//                    <Button theme="primary" label="Settings" onPress={() => navigation.navigate("Settings")} />
+//                </View>
+//                <CircleButton label="Tap To Hear" onPress={onTapToHear} />
+//                <Text style={styles.midText1}>Tap to</Text>
+//                <Text style={styles.midText2}>Hear</Text>
+//                <TextInput style={styles.textBox} value={displayText} multiline={true}/>
+//            </ImageBackground>
+//        </View>
+//    );
+//}
 
 const styles = StyleSheet.create({
     container: {
@@ -66,5 +120,17 @@ const styles = StyleSheet.create({
         paddingLeft: 3,
         textAlign: 'center',
         fontWeight: 'bold',
+    },
+    textBox: {
+      marginTop: 50,
+      marginLeft: 20,
+      padding: 10,
+      borderWidth: 1,
+      borderRadius: 5,
+      borderColor: 'gray',
+      width: '90%',
+      height: 200,
+      backgroundColor: '#fff',
+      textAlignVertical: 'top',
     },
 });
