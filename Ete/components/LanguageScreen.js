a92e6dda5d606bfe7f1a62dab78875198fe57962
nono20794 <chardon@post.bgu.ac.il>
2023-04-14 16:54:42 +0300

    handle saving the new LanguageHeard and LanguageWritten to the db

--- Ete/components/LanguageScreen.js
+++ Ete/components/LanguageScreen.js
@@ -1,6 +1,6 @@
 // Aboutscreen.js
 import React, {useEffect, useState} from "react";
-import {ImageBackground, View, Text, StyleSheet} from "react-native";
+import {ImageBackground, View, Text, StyleSheet, Alert} from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import Button from "../Button"
 import DownButton from "../DownButton"
@@ -12,7 +12,6 @@ export default function LanguageScreen({navigation, route}) {
     const [LanguageHeard, setLanguageHeard] = useState("");
     const [LanguageWritten, setLanguageWritten] = useState("");
     const uid = route.params.uid;
-
     useEffect(() => {
         const userRef = db.ref("users").orderByChild("uid").equalTo(uid);
         const listener = userRef.on("value", (snapshot) => {
@@ -23,6 +22,25 @@ export default function LanguageScreen({navigation, route}) {
         });
         return () => userRef.off("value", listener);
     }, [uid]);
+    const onSave = async () => {
+        try {
+            const uid = route.params.uid;
+            const userRef = db.ref("users");
+            const snapshot = await userRef.orderByChild("uid").equalTo(uid).once("value");
+
+            if (snapshot.exists()) {
+                const userKey = Object.keys(snapshot.val())[0];
+                await userRef.child(userKey).update({
+                    LanguageHeard: LanguageHeard,
+                    LanguageWritten: LanguageWritten
+                });
+            }
+            Alert.alert("saved")
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <View style={styles.container}>
             <ImageBackground source={BackgroundImage} resizeMode="cover" style={styles.image}>
@@ -56,7 +74,7 @@ export default function LanguageScreen({navigation, route}) {
                     </Picker>
                 </View>
                 <View style={styles.footerContainer}>
-                    <DownButton label="Save" />
+                    <DownButton label="Save" onPress={onSave}/>
                     <DownButton label="Cancel" />
                 </View>
             </ImageBackground>
