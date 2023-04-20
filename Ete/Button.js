9ba1bdf0703fee16c5017b69848b1bb7793df7dc
linav <linav@post.bgu.ac.il>
2023-04-20 11:27:36 +0300

    fix screen size issues - not done

--- Ete/Button.js
+++ Ete/Button.js
@@ -1,109 +1,96 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import React from 'react';
+import { View, Pressable, Text, StyleSheet, Dimensions, SafeAreaView } from 'react-native';
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
+
 export default function Button({ label, theme, onPress }) {
-    if (theme === "primary" && label ==="Settings") {
-        return (
-            <View
-                style={[styles.buttonContainer, { borderWidth: 1, borderColor: "#074145", borderRadius: 20 }]}
-            >
-                <Pressable
-                    style={[styles.button]}
-                    onPress={onPress}
+
+
+    //const MyButton = ({ theme, label, onPress }) => {
+        const { height } = Dimensions.get('window');
+        console.log(height);
+        if (theme === "primary" && label ==="Settings") {
+            return (
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={[styles.buttonContainer, { borderWidth: 1, borderColor: '#074145', borderRadius: 20, top: height * 0.01 }]}>
+                        <Pressable style={[styles.button]} onPress={onPress}>
+                            <FontAwesome name='cogs' size={18} color='#074145' style={styles.buttonIcon} />
+                            <Text style={[styles.buttonLabel]}>{label}</Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+            );
+        }
+        else if (theme === "primary" && label ==="Home") {
+            return (
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={[styles.buttonContainer, { borderWidth: 1, borderColor: '#074145', borderRadius: 20, top: height * 0.01 }]}>
+                        <Pressable style={[styles.button]} onPress={onPress}>
+                            <FontAwesome name='home' size={18} color='#074145' style={styles.buttonIcon} />
+                            <Text style={[styles.buttonLabel]}>{label}</Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+            );
+        }
+        else if (theme === "primary" && label ==="Language") {
+            return (
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={[styles.buttonContainer, { borderWidth: 1, borderColor: '#074145', borderRadius: 20, top: height * 0.03 }]}>
+                        <Pressable style={[styles.button]} onPress={onPress}>
+                            <FontAwesome name='globe' size={18} color='#074145' style={styles.buttonIcon} />
+                            <Text style={[styles.buttonLabel]}>{label}</Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+            );
+        }
+        else if (theme === "primary" && label ==="Sign In") {
+            return (
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={[styles.buttonCenter, { borderWidth: 3, borderColor: "#074145", borderRadius: 20, top: height * 0.01 }]}>
+                        <Pressable style={[styles.button]} onPress={onPress}>
+                            <Text style={[styles.buttonLabelCenter]}>{label}</Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+            );
+        }
+        else if (theme === "primary" && label ==="Sign Up") {
+            return (
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={[styles.buttonCenter, { borderWidth: 3, borderColor: "#074145", borderRadius: 20, top: height * 0.01 }]}>
+                        <Pressable style={[styles.button]} onPress={onPress}>
+                            <Text style={[styles.buttonLabelCenter]}>{label}</Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+            );
+        }
+        else if (theme === "primary" && label ==="Log Out") {
+            return (
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={[styles.buttonCenter, { borderWidth: 3,width: '50%', alignSelf: 'center', borderColor: "#074145", borderRadius: 20, bottom: height * 0.05 }]}>
+                        <Pressable style={[styles.button]} onPress={onPress}>
+                            <Text style={[styles.buttonLabelCenter]}>{label}</Text>
+                        </Pressable>
+                    </View>
+                </SafeAreaView>
+                /*<View
+                    style={[styles.buttonCenter, { borderWidth: 3, , borderColor: "#074145", borderRadius: 100, marginBottom:-60 ,marginTop: 5}]}
                 >
-                    <FontAwesome
-                        name="cogs"
-                        size={18}
-                        color="#074145"
-                        style={styles.buttonIcon}
-                    />
-                    <Text style={[styles.buttonLabel]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
-    else if (theme === "primary" && label ==="Home") {
-        return (
-            <View
-                style={[styles.buttonContainer, { borderWidth: 1, borderColor: "#074145", borderRadius: 20 }]}
-            >
-                <Pressable
-                    style={[styles.button]}
-                    onPress={onPress}
-                >
-                    <FontAwesome
-                        name="home"
-                        size={18}
-                        color="#074145"
-                        style={styles.buttonIcon}
-                    />
-                    <Text style={[styles.buttonLabel]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
-    else if (theme === "primary" && label ==="Language") {
-        return (
-            <View
-                style={[styles.buttonContainer, { borderWidth: 1, borderColor: "#074145", borderRadius: 20 }]}
-            >
-                <Pressable
-                    style={[styles.button]}
-                    onPress={onPress}
-                >
-                    <FontAwesome
-                        name="globe"
-                        size={18}
-                        color="#074145"
-                        style={styles.buttonIcon}
-                    />
-                    <Text style={[styles.buttonLabel]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
-    else if (theme === "primary" && label ==="Sign In") {
-        return (
-            <View
-                style={[styles.buttonCenter, { borderWidth: 3, marginHorizontal: '10%', borderColor: "#074145", borderRadius: 20, marginTop: 100, }]}
-            >
-                <Pressable
-                    style={[styles.button]}
-                    onPress={onPress}
-                >
-                <Text style={[styles.buttonLabelCenter]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
-    else if (theme === "primary" && label ==="Sign Up") {
-        return (
-            <View
-                style={[styles.buttonCenter, { borderWidth: 3, marginHorizontal: '-8%', borderColor: "#074145", borderRadius: 20, marginTop: 100, }]}
-            >
-                <Pressable
-                    style={[styles.button]}
-                    onPress={onPress}
-                >
-                <Text style={[styles.buttonLabelCenter]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
-    else if (theme === "primary" && label ==="Log Out") {
-        return (
-            <View
-                style={[styles.buttonCenter, { borderWidth: 3, marginHorizontal: '28%', borderColor: "#074145", borderRadius: 100, marginBottom:-60 ,marginTop: 5}]}
-            >
-                <Pressable
-                    style={[styles.button]}
-                    onPress={onPress}
-                >
-                    <Text style={[styles.buttonLabelCenter]}>{label}</Text>
-                </Pressable>
-            </View>
-        );
-    }
+                    <Pressable
+                        style={[styles.button]}
+                        onPress={onPress}
+                    >
+                        <Text style={[styles.buttonLabelCenter]}>{label}</Text>
+                    </Pressable>
+                </View>*/
+            );
+        }
+        //return null;
+    //};
+
     return (
         <View style={styles.buttonContainer}>
             <Pressable style={styles.button} onPress={onPress}>
@@ -115,15 +102,18 @@ export default function Button({ label, theme, onPress }) {
 
 const styles = StyleSheet.create({
     buttonContainer: {
-        width: '30%',
+        position: 'absolute',
+        width: '90%',
         height: 50,
-        marginHorizontal: '1.5%',
+        alignSelf: 'center',
         alignItems: 'center',
         justifyContent: 'center',
     },
     buttonCenter: {
-        width: '40%',
+        position: 'absolute',
+        width: '90%',
         height: 50,
+        alignSelf: 'center',
         alignItems: 'center',
         justifyContent: 'center',
     },
